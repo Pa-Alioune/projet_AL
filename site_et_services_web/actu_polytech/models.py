34bbed7d3d333d8251b49a502aeed4145e198405
arona <aronadiallo90@gmail.com>
2023-07-08 21:51:53 +0000

    token

--- site_et_services_web/actu_polytech/models.py
+++ site_et_services_web/actu_polytech/models.py
@@ -2,5 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    role = models.CharField(max_length=50)  # Roles: 'visitor', 'editor', 'admin'
+    token = models.CharField(max_length=100, blank=True, null=True)
 
